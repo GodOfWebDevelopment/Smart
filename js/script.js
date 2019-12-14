932a87798e7170ee90e3d61e32808e95a1ed2d9c
God of web development <bindiuua.gmail.com>
2019-12-14 20:58:05 +0200

    Smart

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function openMenu() {
+  document.getElementById("sidebar").classList.toggle("burger-menu_active");
+}
+$(".reviews").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
